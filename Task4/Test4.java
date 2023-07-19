d53f2204abacb267b29b74501f818d57be1ef5d1
Alexandra Shevchenko <alexshevchenko95@gmail.com>
2023-07-19 08:35:51 +0300

    Task4

---
+++ Task4/Test4.java
@@ -0,0 +1,18 @@
+package Task4;
+
+public class Test4 implements MyIf{
+
+
+  @Override
+    public int getUserID() {
+        return 100;
+    }
+
+    @Override
+    public int getAdminID() {
+        return 2;
+    }
+
+
+  
+}
